0d1bff0e9b45446e547dd757e152c7f3f95e7c4b
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-05-29 16:27:34 +0200

    operazioni preliminari JS

---
+++ js/script.js
@@ -0,0 +1,15 @@
+console.log('JS OK')
+
+// Raccolgo gli elemnti dal DOM
+
+const userName = document.getElementById('user-data');
+const distance = document.getElementById('distance');
+const buttonSubmit = document.getElementById('submit');
+
+
+console.log(userName, distance, buttonSubmit)
+
+// Stabilisco la fascia di etò del utente
+
+const userAgeYoung = document.
+
